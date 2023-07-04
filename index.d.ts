6ec03b1535d090fea3714b9245f4f26672dbb686
KeithBrown39423 <keithbrown39423@outlook.com>
2023-07-04 14:22:51 -0500

    Added typescript declaration file
    Added index.d.ts
    Updated version to 2.1.0

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+import session from "express-session";
+import { Low } from "lowdb";
+
+declare module "connect-lowdb" {
+    type LowdbStoreOptions = {
+        db: Low;
+    } & session.SessionOptions;
+
+    export default class LowdbStore extends session.Store {
+        constructor(options?: LowdbStoreOptions);
+        db: Low;
+
+        get(sid: string, cb: (err: any, session?: session.SessionData | null) => any): void;
+        set(sid: string, session: session.Session, cb: (err: any) => any): void;
+        destroy(sid: string, cb: (err: any) => any): void;
+        touch(sid: string, session: session.SessionData, cb: (err: any) => any): void;
+        all(cb: (err: any, obj: session.SessionData[]) => any): void;
+        clear(cb: (err: any) => any): void;
+        length(cb: (err: any, length: number) => any): void;
+    }
+}
